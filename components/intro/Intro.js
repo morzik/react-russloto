abb236acff326ccc30b6b9075d74989ae8cec6b8
t.davydenkova <t.davydenkova@peppers-studio.ru>
2021-03-11 16:36:20 +0300

    chore(Intro, WinnersItems): обновление контента

--- components/intro/Intro.js
+++ components/intro/Intro.js
@@ -6,29 +6,29 @@ import Button from "../button/Button";
 import IntroHeader from "./IntroHeader";
 import {safeHTML} from "../../utils/safeHTML";
 
-const Intro = ({attr, title, subtitle, text, prize, mark, header, button, bg}) => (
-  <section >
-    <div className={"intro"} {...attr}>
-
-      <div className={"intro__content"}>
-        <IntroBg {...bg} />
-        <IntroHeader {...header}/>
-        <div className={"intro__info"}>
-          <h2 className="intro__title">
-            {title}
-          </h2>
-          <p className="intro__prize">{safeHTML(prize)}</p>
-          <p className="intro__subtitle">{subtitle}</p>
-          <mark className="intro__text">
-            <span>{text}</span>
-          </mark>
-          <Button {...button} className={"intro__button"}>{button.text}</Button>
-          <IntroMark {...mark}/>
+const Intro = ({attr, title, subtitle, text, prize, mark, header, button, bg}) => {
+  return (
+    <section >
+      <div className={"intro"} {...attr}>
+        <div className={"intro__content"}>
+          <IntroBg {...bg} />
+          <IntroHeader {...header}/>
+          <div className={"intro__info"}>
+            <h2 className="intro__title">
+              {title}
+            </h2>
+            <p className="intro__prize">{safeHTML(prize)}</p>
+            <p className="intro__subtitle">{subtitle}</p>
+            <mark className="intro__text">
+              <span>{text}</span>
+            </mark>
+            <Button {...button} className={"intro__button"}>{button.text}</Button>
+            <IntroMark {...mark}/>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  )};
 
 
 Intro.propTypes = {

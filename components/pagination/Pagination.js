0353668424322c1a879774fc828ad7a9d37499a3
t.davydenkova <t.davydenkova@peppers-studio.ru>
2021-03-12 15:09:19 +0300

    chore(Pagination): навешивание активного элемента при скролле у удаление слушателя при удалении компонента

--- components/pagination/Pagination.js
+++ components/pagination/Pagination.js
@@ -6,30 +6,40 @@ const Pagination = ({list}) => {
   const [activeId, setActive] = useState(0);
 
   useEffect(() => {
-    const html = document.documentElement;
-    const all = document.querySelectorAll("section");
 
-    function lamdaMiddle(i) {
-      return Math.abs(html.clientHeight / 2 - (all[i].getBoundingClientRect().y + all[i].getBoundingClientRect().height / 2));
-    }
+    function scrollListener (){
+      const html = document.documentElement;
+      const all = document.querySelectorAll("section");
 
-    let min = lamdaMiddle(0);
-    let minI = 0;
-    for (var i = 0; i < all.length; i++) {
-      if (min > lamdaMiddle(i)) {
-        min = lamdaMiddle(i);
-        minI = i;
+      function lamdaMiddle(i) {
+        const {y,height} = all[i].getBoundingClientRect();
+        return Math.abs(html.clientHeight / 2 - (y + height / 2));
       }
+
+      let min = lamdaMiddle(0);
+      let minI = 0;
+      for (let i = 0; i < all.length; i++) {
+        if (min > lamdaMiddle(i)) {
+          min = lamdaMiddle(i);
+          minI = i;
+        }
+      }
+      setActive(minI);
     }
 
-    setActive(minI);
-  });
+     document.addEventListener("scroll", scrollListener);
 
-  const listItems = list.map(({href},index) => (
-    <li className={`pagination__item ${activeId === index?"pagination__item_active":""}`} key={index}>
-      <a href={href}/>
+     return () => {
+       document.removeEventListener("scroll", scrollListener);
+     }
+  },[]);
+
+
+  const listItems = list.map(({href}, index) => (
+    <li className={`pagination__item ${activeId === index ? "pagination__item_active" : ""}`} key={index}>
+      <a href={href} />
     </li>
-  ))
+  ));
   return (
     <nav className={"pagination"}>
       <ul className="pagination__list">

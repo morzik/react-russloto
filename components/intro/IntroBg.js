75a19b99f341f7d62c040ea1060ea91f02703730
t.davydenkova <t.davydenkova@peppers-studio.ru>
2021-03-16 11:42:27 +0300

    chore(steam): дым для мужика

--- components/intro/IntroBg.js
+++ components/intro/IntroBg.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Picture from "../picture/Picture";
+import Steam from "../steam/Steam";
 
 const IntroBg = ({person,table}) => (
   <div className={"intro__bg"}>
     <div className={"intro__bg-block"}>
       <div className={"intro__bg-person"}>
         <Picture {...person}/>
+        <Steam className={"intro__steam intro__steam_1"}/>
+        <Steam className={"intro__steam intro__steam_2"}/>
       </div>
       <div className={"intro__bg-table"}>
         <Picture {...table}/>
---
+++ components/steam/Steam.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Steam = ({className}) => (
+  <div className={`steam ${className}`}>
+    <div className="steam__item steam__item_1"></div>
+    <div className="steam__item steam__item_2"></div>
+    <div className="steam__item steam__item_3"></div>
+    <div className="steam__item steam__item_4"></div>
+    <div className="steam__item steam__item_5"></div>
+    <div className="steam__item steam__item_6"></div>
+  </div>
+);
+
+export default Steam;
+

5bccadff8ee973c603228338e28303605d1f4636
t.davydenkova <t.davydenkova@peppers-studio.ru>
2021-03-23 16:43:09 +0300

    chore(steam): чистка стилей

--- components/steam/Steam.js
+++ components/steam/Steam.js
@@ -1,16 +1,18 @@
 import React from "react";
 
 
-const Steam = ({className}) => (
-  <div className={`steam ${className}`}>
-    <div className="steam__item steam__item_1"></div>
-    <div className="steam__item steam__item_2"></div>
-    <div className="steam__item steam__item_3"></div>
-    <div className="steam__item steam__item_4"></div>
-    <div className="steam__item steam__item_5"></div>
-    <div className="steam__item steam__item_6"></div>
-  </div>
-);
+const Steam = ({className}) => {
+  return (
+    <div className={`steam ${className}`}>
+      <div className="steam__item steam__item_1"/>
+      <div className="steam__item steam__item_2"/>
+      <div className="steam__item steam__item_3"/>
+      <div className="steam__item steam__item_4"/>
+      <div className="steam__item steam__item_5"/>
+      <div className="steam__item steam__item_6"/>
+    </div>
+  )
+};
 
 export default Steam;
 
